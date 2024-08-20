cd44e292862d197f72db7225e76d428e4ffe46c8
Matej Liskovec <matej.liskovec@admiral.at>
2024-08-20 21:35:53 +0200

    hask is importand

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), // Enable hash-based routing
     { provide: BrowserAnimationsModule, useValue: BrowserAnimationsModule },
   ],
 }).catch((err) => console.error(err));
