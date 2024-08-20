b80f67faef3a9ff9d100fa7c84f168368370df02
Matej Liskovec <matej.liskovec@admiral.at>
2024-08-20 21:17:56 +0200

    aa

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
+  imports: [RouterLink],
 })
 export class NavbarComponent {}
