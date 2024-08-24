3dbd5bff55d25d328c8185cd2d058eb2753d1dee
liskovec_matej <matej.liskovec@gmail.com>
2024-08-24 16:06:53 +0200

    obhlaidka update

--- src/app/components/wedding-tour/wedding-tour.component.ts
+++ src/app/components/wedding-tour/wedding-tour.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-wedding-tour',
-  standalone: true,
-  imports: [],
   templateUrl: './wedding-tour.component.html',
-  styleUrl: './wedding-tour.component.scss'
+  styleUrls: ['./wedding-tour.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state(
+        'hidden',
+        style({
+          left: '100%',
+          transform: 'translateX(-100%)',
+        })
+      ),
+      state(
+        'visible',
+        style({
+          left: '50%',
+          transform: 'translateX(-50%)',
+        })
+      ),
+      transition('hidden => visible', [animate('1s')]), // Adjusted duration for better mobile experience
+    ]),
+  ],
 })
 export class WeddingTourComponent {
+  photoStates = ['hidden', 'hidden', 'hidden'];
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const elements = document.querySelectorAll('.photo-background');
+    elements.forEach((element, index) => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        this.photoStates[index] = 'visible';
+      }
+    });
+  }
+
+  @HostListener('window:resize', [])
+  onWindowResize() {
+    this.onWindowScroll(); // Recalculate visibility on resize
+  }
 }
